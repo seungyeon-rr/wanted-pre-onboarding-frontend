ba16533c9e3b5b21a13b1ee109995d961bcab37f
seungyeon-rr <syryu926@gmail.com>
2022-10-20 00:14:14 +0900

    Fix : 배포 에러 수정

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from './Router';
 import 'bootstrap/dist/css/bootstrap.css';
-import './styles/common.scss';
+import './styles/common.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Router />);
